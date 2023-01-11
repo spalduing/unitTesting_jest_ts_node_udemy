ceb8ee630d18db5a28fabde1b46bf6f3bd0c7b4e
Miguelangel Diaz <Miguel.Diaz@jkmoving.com>
2023-01-11 08:58:25 -0500

    Testing random: spyOn global objects

--- src/test/Authorization/Authorizer.test.ts
+++ src/test/Authorization/Authorizer.test.ts
@@ -1,18 +1,71 @@
-import { Authorizer } from "../../app/Authorization/Authorizer"
+import { Authorizer } from "../../app/Authorization/Authorizer";
 import { SessionTokenDBAccess } from "../../app/Authorization/SessionTokenDBAccess";
 import { UserCredentialsDbAccess } from "../../app/Authorization/UserCredentialsDbAccess";
+import {
+  AccessRight,
+  Account,
+  SessionToken,
+  UserCredentials,
+} from "../../app/Models/ServerModels";
 
-jest.mock('../../app/Authorization/SessionTokenDBAccess');
-jest.mock('../../app/Authorization/UserCredentialsDbAccess');
+jest.mock("../../app/Authorization/SessionTokenDBAccess");
+jest.mock("../../app/Authorization/UserCredentialsDbAccess");
 
-describe('Authorizer test suite', ()=>{
-    let authorizer: Authorizer;
+const someAccount: Account = {
+    username: "someUser",
+    password: "password",
+  };
 
-    test('constructor arguments', ()=>{
-        new Authorizer();
-        expect(SessionTokenDBAccess).toBeCalled();
-        expect(UserCredentialsDbAccess).toBeCalled();
-        
+describe("Authorizer test suite", () => {
+  let authorizer: Authorizer;
+
+  const sessionTokenDBAccessMock = {
+    storeSessionToken: jest.fn(),
+  };
+  const userCredentialsDBAccessMock = {
+    getUserCredential: jest.fn(),
+  };
+
+  beforeEach(() => {
+    authorizer = new Authorizer(
+      sessionTokenDBAccessMock as any,
+      userCredentialsDBAccessMock as any
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test("constructor arguments", () => {
+    new Authorizer(
+    );
+    expect(SessionTokenDBAccess).toBeCalled();
+    expect(UserCredentialsDbAccess).toBeCalled();
+  });
+
+
+  test("should return session token for valid credentials", async () => {
+    jest.spyOn(global.Math, "random").mockReturnValueOnce(0);
+    jest.spyOn(global.Date, "now").mockReturnValueOnce(0);
+
+    userCredentialsDBAccessMock.getUserCredential.mockReturnValue({
+      accessRights: [AccessRight.CREATE, AccessRight.READ],
+      username: "someUser",
+      password: "password",
     });
 
-})
+    const expectedSessionToken: SessionToken = {
+      accessRights: [AccessRight.CREATE, AccessRight.READ],
+      userName: "someUser",
+      valid: true,
+      expirationTime: new Date(60 * 60 * 1000),
+      tokenId: "",
+    };
+
+    const sessionToken = await authorizer.generateToken(someAccount);
+
+    expect(sessionToken).toEqual(expectedSessionToken);
+    expect(sessionTokenDBAccessMock.storeSessionToken).toHaveBeenCalledWith(sessionToken);
+  });
+});
